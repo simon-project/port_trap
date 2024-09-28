c2990da937ee8f3d8cf917556f9e7bbbc650b0dc
simon <simon@fastvps.ru>
2024-09-29 00:33:27 +0300

    Fix stop service

--- stop_traps.sh
+++ stop_traps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 logger -t port_trap.py "Disable port traps..."
 echo "Current processes:";
-ps auxfww | grep -v grep| grep port_trap.py
-for i in $(ps auxfww | grep -v grep| grep port_trap.py | awk '{print $2}'); do kill -15 $i; echo "Stoped PID: ${i}"; done
+ps auxfww | grep -vE "grep|systemctl|journalctl"| grep port_trap.py
+for i in $(ps auxfww | grep -vE "grep|systemctl|journalctl"| grep port_trap.py | awk '{print $2}'); do kill -15 $i; echo "Stoped PID: ${i}"; done
 logger -t port_trap.py "Port traps disabled."
